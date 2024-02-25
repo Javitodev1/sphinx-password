ab6bf7f477bdc8f53f6c1ffff943c56a141b7029
Javier Fuentes <javito.dev@outlook.com>
2024-02-25 14:57:26 -0300

    gitignore updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import robotsTxt from 'astro-robots-txt';
-import { astroImageTools } from 'astro-imagetools';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import robotsTxt from 'astro-robots-txt'
+import { astroImageTools } from 'astro-imagetools'
 
 // https://astro.build/config
 export default defineConfig({
@@ -24,4 +24,4 @@ export default defineConfig({
     robotsTxt(),
     astroImageTools,
   ],
-});
+})
